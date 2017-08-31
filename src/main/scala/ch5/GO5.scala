6806a0a52799c3a055b221d92064515178f3a11a
eoconnor <eoin.oconnor@zalando.ie>
2017-08-31 10:23:24 +0100

    work on Tree

--- src/main/scala/ch5/GO5.scala
+++ src/main/scala/ch5/GO5.scala
@@ -2,6 +2,7 @@ package ch5
 
 import ch5.ErrorHandling.{Failure, Success}
 import ch5.LinkedList._
+import ch5.Tree._
 
 object GO5 extends App{
 
@@ -15,5 +16,13 @@ object GO5 extends App{
   assert(example2(1) == Success(2))
   assert(example2(2) == Success(3))
   assert(example2(3) == Failure("Bad times dude"))
+
+  val tree: Tree[String] =
+    Node(Node(Leaf("To"), Leaf("iterate")),
+      Node(Node(Leaf("is"), Leaf("human,")),
+        Node(Leaf("to"), Node(Leaf("recurse"), Leaf("divine")))))
+
+
+  println(tree.fold[String]((a, b) => a + " " + b, str => str))
 
 }
